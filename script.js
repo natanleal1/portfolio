5b974c4abf37af4d2cf59283615725d77fdd67df
natanleal1 <naloliveira216@gmail.com>
2025-06-12 21:47:59 -0300

    languagesUpdate

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-let menuIcon = document.querySelector(".fa-bars");
 let navbar = document.querySelector(".navbar");
+let lightMode = document.querySelector(".fa-gear");
+let menuIcon = document.querySelector(".fa-bars");
 
 menuIcon.onclick = () =>{
-    menuIcon.classList.toggle("bx-x");
-    navbar.classList.toggle("active");
+  //menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
 }
+lightMode.addEventListener("click", () => {
+  document.body.classList.toggle("light");
+});
